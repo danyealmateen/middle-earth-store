f6a2f9f00648e5ef9722a6848386d13f2f3d5594
danyealmateen <danyeal.mateen@hotmail.com>
2023-09-20 16:47:58 +0200

    fixed stock update against firebase db

--- src/BuyWeapons/GetWeapons.tsx
+++ src/BuyWeapons/GetWeapons.tsx
@@ -1,15 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Weapon } from '../types';
 import './GetWeapons.css';
-import { Link } from 'react-router-dom';
 
 const GetWeapons = () => {
   const [weapons, setWeapons] = useState<Record<string, Weapon> | null>(null);
-  const [addtoCart, setAddToCart] = useState(0);
 
-  // const currentStock = () => {
-  //   setAddToCart(value.stock - 1);
-  // };
+  const handleAddToCart = async (key: string) => {
+    console.log('key:', key);
+
+    if (weapons && weapons[key] && weapons[key].stock > 0) {
+      const weaponToUpdate = { ...weapons[key] };
+      weaponToUpdate.stock -= 1;
+
+      try {
+        const response = await fetch(
+          `https://theelvenforgeshop-default-rtdb.europe-west1.firebasedatabase.app/${key}.json`,
+          {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(weaponToUpdate),
+          }
+        );
+
+        if (response.ok) {
+          const updatedWeapons = { ...weapons };
+          updatedWeapons[key] = weaponToUpdate;
+          setWeapons(updatedWeapons);
+        } else {
+          console.error('fail');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,9 +43,7 @@ const GetWeapons = () => {
         const response = await fetch(
           'https://theelvenforgeshop-default-rtdb.europe-west1.firebasedatabase.app/.json'
         );
-
         const data = await response.json();
-
         setWeapons(data);
       } catch (error) {
         console.log(error);
@@ -31,20 +55,27 @@ const GetWeapons = () => {
   return (
     <div className='weapon-card-container'>
       {weapons &&
-        Object.entries(weapons).map(([key, value]) => (
-          <div key={key}>
-            <div className='weapon-card'>
-              <img className='weapon-card-img' src={value.img} alt='' />
-              <p>{value.title}</p>
-              <p>{value.description}</p>
-              <p>Price: {value.price} gold</p>
-              <p>Stock: {value.stock} left</p>
-              <button onClick={() => setAddToCart(value.stock - 1)}>
-                Add to cart
-              </button>
+        Object.entries(weapons).map(([key, value]) => {
+          console.log('Current weapon key:', key);
+          return (
+            <div key={key}>
+              <div className='weapon-card'>
+                <img className='weapon-card-img' src={value.img} alt='' />
+                <p>{value.title}</p>
+                <p>{value.description}</p>
+                <p>Price: {value.price} gold</p>
+                <p>Stock: {value.stock} left</p>
+                <button
+                  onClick={() => {
+                    handleAddToCart(key);
+                  }}
+                >
+                  Add to cart
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
     </div>
   );
 };
